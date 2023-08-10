647093725aef9c547b435db72c1996261644ed44
salsal07 <salsalvm@real11.com>
2023-08-10 15:58:23 +0530

    spin functionality completed

--- app/src/main/java/com/sample/spinningwheel/MainActivity.kt
+++ app/src/main/java/com/sample/spinningwheel/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import com.sample.spinningwheel.databinding.ActivityMainBinding
 import java.util.*
@@ -13,99 +14,79 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private var timer: CountDownTimer? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val random = Random()
 
 
-        binding.btnTarget.setOnClickListener(View.OnClickListener {
-            // disabling the button so that user
-            // should not click on the button
-            // while the wheel is spinning
+
+        binding.btnTarget.setOnClickListener {
+            //here refreshing rotation and button status
+            binding.icBoard.rotation = 0f
             binding.btnTarget.isEnabled = false
 
-            // reading random value between 10 to 30
-            var spin: Int = 8
-//                random.nextInt(10) + 10
 
-            Log.d("TAG spin", "onCreate: $spin ")
-            // since the wheel has 10 divisions, the
-            // rotation should be a multiple of
-            // 360/10 = 36 degrees
-            spin *= 36
+            val findAngle: Int = findAngleReward("spin_again")
 
-            // timer for each degree movement
-            timer = object : CountDownTimer((spin * 10).toLong(), 1) {
+
+            // Calculate the angle needed to reach the static value (certain degrees)
+            //4 round static 1440 plus backend data
+            var staticValueAngle = 1440 + findAngle
+
+
+            timer = object : CountDownTimer((10000).toLong(), 1) {
                 override fun onTick(l: Long) {
-                    // rotate the wheel
-                    val rotation = binding.icBoard.rotation + 2
-                    binding.icBoard.rotation = rotation
 
+                    //rotation speed add here
+                    binding.icBoard.rotation = speedControl(binding.icBoard.rotation, findAngle)
 
-                    Log.d("TAG spin", "onCreate: $rotation ")
+                    // Check if the current rotation angle reaches the  value angle
+                    if (binding.icBoard.rotation >= staticValueAngle) {
+                        timer?.cancel()
+                        binding.btnTarget.isEnabled = true // Enable the button
+                    }
                 }
 
-
-
-
                 override fun onFinish() {
-                    // enabling the button again
                     binding.btnTarget.isEnabled = true
                 }
             }.start()
-        })
+        }
+
 
     }
+
+    private fun findAngleReward(findAngle: String): Int {
+
+        return when (findAngle) {
+            "50_coin" -> 360
+            "20_coin" -> 315
+            "10_coin" -> 135
+            "better_luck" -> 90
+            "spin_again" -> 180
+            "10_coin_second" -> 270
+            "better_luck_second" -> 225
+            "5_coin" -> 45
+
+            else -> 90
+        }
+    }
+
+
+    private fun speedControl(rotation: Float, findAngle: Int): Float {
+        return if (rotation > 1440 + findAngle / 2) {
+            binding.icBoard.rotation + 2
+        } else if (rotation > 1440) {
+            binding.icBoard.rotation + 3
+        } else if (rotation > 1080) {
+            binding.icBoard.rotation + 6
+        } else if (rotation > 720) {
+            binding.icBoard.rotation + 9
+        } else {
+            binding.icBoard.rotation + 15
+        }
+    }
+
 }
-
-
-/*
-class MainActivity : AppCompatActivity() {
-    var btnSpin: Button? = null
-    var ivWheel: ImageView? = null
-    var timer: CountDownTimer? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.)
-
-        // initializing views
-        btnSpin = findViewById<Button>(R.id.btn)
-        ivWheel = findViewById<ImageView>(R.id.ivWheel)
-
-        // creating an object of Random class
-        // to generate random numbers for the spin
-        val random = Random()
-
-        // on click listener for btnSpin
-        btnSpin.setOnClickListener(View.OnClickListener {
-            // disabling the button so that user
-            // should not click on the button
-            // while the wheel is spinning
-            btnSpin.setEnabled(false)
-
-            // reading random value between 10 to 30
-            var spin = random.nextInt(20) + 10
-
-            // since the wheel has 10 divisions, the
-            // rotation should be a multiple of
-            // 360/10 = 36 degrees
-            spin = spin * 36
-
-            // timer for each degree movement
-            timer = object : CountDownTimer((spin * 20).toLong(), 1) {
-                override fun onTick(l: Long) {
-                    // rotate the wheel
-                    val rotation = ivWheel.getRotation() + 2
-                    ivWheel.setRotation(rotation)
-                }
-
-                override fun onFinish() {
-                    // enabling the button again
-                    btnSpin.setEnabled(true)
-                }
-            }.start()
-        })
-    }
-}*/
